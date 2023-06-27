e883b37b0cb2a0ab344b1af0b67bcd4f4c3096c2
mvfranzke <michellevonfranzke@gmail.com>
2023-06-26 21:54:19 -0700

    Setting middleware to parse url, json bodies and static file from the public directory

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,9 +7,10 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
-app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use(express.static("public")); // Serving static files from the "public" directory
+//Setting middleware to parse url, json bodies and static file from the public directory
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+app.use(express.static("public")); 
 
 app.use("/api", apiRoutes); // Using the apiRoutes module for routes starting with "/api"
 app.use("/", htmlRoutes); // Using the htmlRoutes module for routes starting with "/"
